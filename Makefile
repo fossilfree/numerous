e67fe432fa7496d2b3506f0fc06d06c5b9c2115d
mfc <cristian.marius.florin@protonmail.com>
2019-12-28 18:46:39 +0100

    makerule for benchmark

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ install:
 
 run-tests:
 	python3 -m pytest
+
+run-benchmark:
+	python3 ./benchmark/tst.py
--- benchmark/tst.py
+++ benchmark/tst.py
@@ -1,9 +1,9 @@
 import random
 import time
 
-from engine.model import Model
-from engine.simulation import Simulation
-from engine.system import Item, ConnectorTwoWay, Subsystem
+from numerous.engine.model import Model
+from numerous.engine.simulation import Simulation
+from numerous.engine.system import Item, ConnectorTwoWay, Subsystem
 from numerous import Equation
 from numerous.engine import OverloadAction
 from numerous.multiphysics import equation
