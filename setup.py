e73151dc340c83e96fb92a8692c196509e59913b
artem <artem.chupryna@energymachines.com>
2020-01-27 16:45:15 +0100

    v 0.0.11

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="numerous-engine",
-    version="0.0.10",
+    version="0.0.11",
     author='Artem Chupryna, EnergyMachines ApS',
     author_email='artem.chupryna@energymachines.com',
     description="Numerous  - an object-oriented modelling and simulation engine.",
