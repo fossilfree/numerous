4002fce2fa1b9c1e8921534908254852ac31c7ce
artem <artem.chupryna@energymachines.com>
2020-06-11 14:39:28 +0200

    mapping optimization. Now equations are sorted according to number of mappings they have.

--- numerous/engine/model/equation_parser.py
+++ numerous/engine/model/equation_parser.py
@@ -11,29 +11,31 @@ class Equation_Parser():
     def __init__(self):
         pass
 
-    def get_complex_id(self,eq_dict):
+    def get_complex_id(self, eq_dict):
         id = ""
         for i in range(len(eq_dict)):
             id += eq_dict[i].id
         return id
 
-
     def parse(self, model):
         compiled_equations_idx = []
         compiled_eq = []
+        compiled_eq_m = []
         compiled_equations_ids = []  # As equations can be non-unique, but they should?
         for tt in model.synchronized_scope.keys():
             eq_text = ""
             eq_id = ""
             # id of eq in list of eq)
-            if model.equation_dict[tt]:
+            if model.equation_dict[tt][0]:
                 eq_text_2 = ""
-                if self.get_complex_id(model.equation_dict[tt]) in compiled_equations_ids:
-                    compiled_equations_idx.append(compiled_equations_ids.index(self.get_complex_id(model.equation_dict[tt])))
+                eq = model.equation_dict[tt][0]
+                eq_out_m = model.equation_dict[tt][1]
+                if self.get_complex_id(eq) in compiled_equations_ids:
+                    compiled_equations_idx.append(compiled_equations_ids.index(self.get_complex_id(eq)))
                     continue
-                eq_id = self.get_complex_id(model.equation_dict[tt])
-                for i in range(len(model.equation_dict[tt])):
-                    lines = model.equation_dict[tt][i].lines.split("\n")
+                eq_id = self.get_complex_id(eq)
+                for i in range(len(eq)):
+                    lines = eq[i].lines.split("\n")
 
                     non_empty_lines = [line for line in lines if line.strip() != ""]
 
@@ -77,6 +79,7 @@ class Equation_Parser():
                     eq_text = "def eq_body():\n   from numerous.engine.model.simple_vectorizer import simple_vectorize\n   import numpy as np\n" + eq_text
             else:
                 eq_id = "empty_equation"
+                eq_out_m = 0
                 if eq_id in compiled_equations_ids:
                     compiled_equations_idx.append(compiled_equations_ids.index(eq_id))
                     continue
@@ -89,10 +92,10 @@ class Equation_Parser():
             compiled_equations_idx.append(len(compiled_equations_ids))
             compiled_equations_ids.append(eq_id)
             compiled_eq.append(list(namespace.values())[1]())
+            compiled_eq_m.append(eq_out_m)
 
-        return np.array(compiled_eq), np.array(compiled_equations_idx, dtype=np.int64)
-
-
+        return np.array(compiled_eq), np.array(compiled_equations_idx, dtype=np.int64), np.argsort(
+            np.array(compiled_eq_m))
 
     @staticmethod
     def parse_non_numba_function(function: tp.Callable, decorator_type: str, ) -> tp.Callable:
@@ -129,19 +132,30 @@ class Equation_Parser():
 
     @staticmethod
     def create_numba_iterations(numba_model_class, list_of_functions: tp.List[tp.Callable], callable_method_name: str,
-                                internal_method_name:str, iteration_block: tp.Callable,callable_parameters:str) -> None:
+                                internal_method_name: str, iteration_block: tp.Callable, callable_parameters: str,
+                                map_sorting=None) -> None:
         code_as_string = ""
-        if len(list_of_functions)>0:
-            for i, function in enumerate(list_of_functions):
-                method_name = internal_method_name + str(i)
-                setattr(numba_model_class, method_name, function)
-                code_as_string += iteration_block(method_name, i)
-            code_as_string = "def eval():\n   def " + callable_method_name + "(self,"+callable_parameters+"):\n"\
-                             + code_as_string \
-                             + "   return " + callable_method_name
+        if len(list_of_functions) > 0:
+            if map_sorting is not None:
+                if len(map_sorting) > 0:
+                    for j in map_sorting:
+                        method_name = internal_method_name + str(j)
+                        setattr(numba_model_class, method_name, list_of_functions[j])
+                        code_as_string += iteration_block(method_name, j)
+                    code_as_string = "def eval():\n   def " + callable_method_name + "(self," + callable_parameters + "):\n" \
+                                     + code_as_string \
+                                     + "   return " + callable_method_name
+            else:
+                for i, function in enumerate(list_of_functions):
+                    method_name = internal_method_name + str(i)
+                    setattr(numba_model_class, method_name, function)
+                    code_as_string += iteration_block(method_name, i)
+                code_as_string = "def eval():\n   def " + callable_method_name + "(self," + callable_parameters + "):\n" \
+                                 + code_as_string \
+                                 + "   return " + callable_method_name
         else:
-            code_as_string = "def eval():\n   def " + callable_method_name\
-                             + "(self,"+callable_parameters+"):\n      pass\n   return " + callable_method_name
+            code_as_string = "def eval():\n   def " + callable_method_name \
+                             + "(self," + callable_parameters + "):\n      pass\n   return " + callable_method_name
 
         tree = ast.parse(code_as_string, mode='exec')
         code = compile(tree, filename='test', mode='exec')
