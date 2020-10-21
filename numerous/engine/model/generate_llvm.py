363f4a1ac35516139fec073728db24d0c0ffe9cd
artem <artem.chupryna@energymachines.com>
2020-10-21 16:19:37 +0200

    work in progress 21/10 -2

--- numerous/engine/model/generate_llvm.py
+++ numerous/engine/model/generate_llvm.py
@@ -20,7 +20,7 @@ ee = llvm.create_mcjit_compiler(llvmmodule, target_machine)
 
 class LLVMGenerator:
 
-    def __init__(self, variables, variable_values, n_deriv, n_var):
+    def __init__(self, variables, variable_values, n_var):
         self.detailed_print('target data: ', target_machine.target_data)
         self.detailed_print('target triple: ', target_machine.triple)
         self.module = ll.Module()
@@ -29,27 +29,24 @@ class LLVMGenerator:
         self.fnty = ll.FunctionType(ll.DoubleType().as_pointer(), [
             ll.DoubleType().as_pointer()
         ])
-
+        self.index0 = 0
         self.fnty.args[0].name = "y"
 
-        max_deriv = np.int64(n_deriv)
-        max_var = np.int64(n_var)
+        self.max_var = np.int64(n_var)
 
-        var_global = ll.GlobalVariable(self.module, ll.ArrayType(ll.DoubleType(), max_var), 'global_var')
-        var_global.initializer = ll.Constant(ll.ArrayType(ll.DoubleType(), max_var),
-                                             [float(v) for v in variable_values])
+        self.variables = variables
 
+        self.var_global = ll.GlobalVariable(self.module, ll.ArrayType(ll.DoubleType(), self.max_var), 'global_var')
+        self.var_global.initializer = ll.Constant(ll.ArrayType(ll.DoubleType(), self.max_var),
+                                                  [float(v) for v in variable_values])
+        ##Fixed structure of the kernel
         func = ll.Function(self.module, self.fnty, name="kernel")
-
         self.bb_entry = func.append_basic_block(name='entry')
         self.bb_loop = func.append_basic_block(name='main')
         self.bb_store = func.append_basic_block(name='store')
         self.bb_exit = func.append_basic_block(name='exit')
-
         self.builder = ll.IRBuilder()
-
         self.builder.position_at_end(self.bb_entry)
-
         self.index0 = ll.IntType(64)(0)
 
         self.values = {}
@@ -76,37 +73,33 @@ class LLVMGenerator:
 
         self.ext_funcs[name] = f_llvm
 
-    def generate(self, program, functions, ix_d):
-        t1 = time()
-
+    def generate(self, program, functions, ix_d, n_deriv):
         for function in functions:
             self.add_external_function(function)
 
         # Define global variable array
-
         self.builder.branch(self.bb_loop)
         self.builder.position_at_end(self.bb_loop)
 
-        poplist = []
         single_arg_sum_counter = 0
         for ix, p in enumerate(program):
 
             if 'args' in p:
-                args = [self.builder.load(values[a], 'arg_' + a) for a in p['args']]
+                args = [self.builder.load(self.values[a], 'arg_' + a) for a in p['args']]
 
             target_pointers = []
             if 'targets' in p:
 
                 for t in p['targets']:
-                    index = ll.IntType(64)(variables.index(t))
+                    index = ll.IntType(64)(self.variables.index(t))
 
-                    ptr = var_global
-                    indices = [index0, index]
+                    ptr = self.var_global
+                    indices = [self.index0, index]
 
                     eptr = self.builder.gep(ptr, indices, name=t)
 
                     target_pointers.append(eptr)
-                    values[t] = eptr
+                    self.values[t] = eptr
 
             if p['func'] == 'call':
 
@@ -119,11 +112,11 @@ class LLVMGenerator:
                 elif la == 1:
                     single_arg_sum_counter += 1
                     for t in p['targets']:
-                        self.builder.store(args[0], values[t])
+                        self.builder.store(args[0], self.values[t])
                 elif la == 2:
-                    sum_ = builder.fadd(args[0], args[1])
+                    sum_ =  self.builder.fadd(args[0], args[1])
                     for t in p['targets']:
-                        self.builder.store(sum_, values[t])
+                        self.builder.store(sum_, self.values[t])
 
                 else:
                     accum = self.builder.fadd(args[0], args[1])
@@ -131,7 +124,7 @@ class LLVMGenerator:
                         accum = self.builder.fadd(accum, a)
 
                     for t in p['targets']:
-                        self.builder.store(accum, values[t])
+                        self.builder.store(accum, self.values[t])
 
         self.detailed_print('single assignments: ', single_arg_sum_counter)
 
@@ -147,10 +140,92 @@ class LLVMGenerator:
 
         indexd = ll.IntType(64)(ix_d)
 
-        dg_ptr = self.builder.gep(var_global, [index0, indexd])
+        dg_ptr = self.builder.gep(self.var_global, [self.index0, indexd])
 
         self.builder.ret(dg_ptr)
         # Vars function
+
+        self.save_module('llvm_IR_code.txt')
+
+        llmod = llvm.parse_assembly(str(self.module))
+
+        pmb = llvm.create_pass_manager_builder()
+        pmb.opt_level = 1
+        pm = llvm.create_module_pass_manager()
+        pmb.populate(pm)
+
+        pm.run(llmod)
+
+        # self.detailed_print("-- optimize:", t6 - t5)
+
+        ee.add_module(llmod)
+
+        ee.finalize_object()
+        cfptr = ee.get_function_address("kernel")
+        cfptr_var = ee.get_function_address("vars")
+
+
+        c_float_type = type(np.ctypeslib.as_ctypes(np.float64()))
+        self.detailed_print(c_float_type)
+
+        diff_ = CFUNCTYPE(POINTER(c_float_type), POINTER(c_float_type))(cfptr)
+
+        vars_ = CFUNCTYPE(POINTER(c_float_type), c_int64)(cfptr_var)
+
+        @njit('float64[:](float64[:])')
+        def diff(y):
+
+            deriv_pointer = diff_(y.ctypes)
+            return carray(deriv_pointer, (n_deriv,)).copy()
+
+        @njit('float64[:](float64)')
+        def variables__(f):
+            variables_pointer = vars_(0)
+            variables_array = carray(variables_pointer, (self.max_var,))
+
+            return variables_array.copy()
+
+        return diff, variables__
+
+    def detailed_print(self, *args, sep=' ', end='\n', file=None):
+        if config.PRINT_LLVM:
+            print(*args, sep, end, file)
+
+    def save_module(self, filename):
+        with open(filename, 'w') as f:
+            f.write(str(self.module))
+
+    def add_load(self, sequence, name):
+        for ix, v in enumerate(sequence):
+            index = ll.IntType(64)(ix)
+
+            if name == 'variables':
+                ptr = self.var_global
+                indices = [self.index0, index]
+
+            elif name == 'y':
+                ptr = self.func.args[0]
+                indices = [index]
+
+            eptr = self.builder.gep(ptr, indices, name + "_" + v)
+            self.values[v] = eptr
+
+    def add_store(self, sequence, name):
+        for ix, v in enumerate(sequence):
+            index = ll.IntType(64)(ix)
+
+            if name == 'variables':
+                ptr = self.var_global
+                indices = [self.index0, index]
+
+            eptr = self.builder.gep(ptr, indices)
+            self.builder.store(self.builder.load(self.values[v]), eptr)
+
+    def add_call(self):
+        pass
+
+
+    def build_var(self):
         fnty_vars = ll.FunctionType(ll.DoubleType().as_pointer(), [ll.IntType(64)])
 
         vars_func = ll.Function(self.module, fnty_vars, name="vars")
@@ -166,121 +241,9 @@ class LLVMGenerator:
         builder_var.position_at_end(bb_exit_var)
 
         index0_var = ll.IntType(64)(0)
-        vg_ptr = builder_var.gep(var_global, [index0_var, index0_var])
+        vg_ptr = builder_var.gep(self.var_global, [index0_var, index0_var])
         builder_var.ret(vg_ptr)
 
-        t2 = time()
-
-        self.detailed_print("-- generate IR:", t2 - t1)
-
-        t3 = time()
-
-        self.save_module('llvm_IR_code.txt')
-
-        llmod = llvm.parse_assembly(str(self.module))
-
-        t4 = time()
-
-        self.detailed_print("-- parse assembly:", t4 - t3)
-
-        pmb = llvm.create_pass_manager_builder()
-        pmb.opt_level = 1
-        pm = llvm.create_module_pass_manager()
-        pmb.populate(pm)
-
-        t5 = time()
-
-        pm.run(llmod)
-
-        t6 = time()
-
-        self.detailed_print("-- optimize:", t6 - t5)
-
-        t7 = time()
-
-        ee.add_module(llmod)
-
-        ee.finalize_object()
-        cfptr = ee.get_function_address("kernel")
-        cfptr_var = ee.get_function_address("vars")
-
-        t8 = time()
-        self.detailed_print("-- JIT compile:", t8 - t7)
-
-        c_float_type = type(np.ctypeslib.as_ctypes(np.float64()))
-        self.detailed_print(c_float_type)
-
-        diff_ = CFUNCTYPE(POINTER(c_float_type), POINTER(c_float_type))(cfptr)
-
-        vars_ = CFUNCTYPE(POINTER(c_float_type), c_int64)(cfptr_var)
-
-        @njit('float64[:](float64[:])')
-        def diff(y):
-
-            deriv_pointer = diff_(y.ctypes)
-
-            return carray(deriv_pointer, (n_deriv,)).copy()
-
-        @njit('float64[:](float64[:],int64)')
-        def variables_(var, set_):
-            print("test1")
-            variables_pointer = vars_(0)
-            variables_array = carray(variables_pointer, (max_var,))
-
-            if set_ > 0:
-                if len(var) == max_var:
-
-                    variables_array[:] = var[:]
-                else:
-                    raise ValueError('bad length!')
-
-            return variables_array
-
-        @njit('float64[:](float64)')
-        def variables__(f):
-            variables_pointer = vars_(0)
-            variables_array = carray(variables_pointer, (max_var,))
-
-            return variables_array.copy()
-
-        return diff, variables__, variables_, max_deriv
-
-    def detailed_print(self, *args, sep=' ', end='\n', file=None):
-        if config.PRINT_LLVM:
-            print(*args, sep, end, file)
-
-    def save_module(self, filename):
-        with open(filename, 'w') as f:
-            f.write(str(self.module))
-
-    def add_load(self, sequence, name):
-        for ix, v in enumerate(sequence):
-            index = ll.IntType(64)(ix)
-
-            if name == 'variables':
-                ptr = var_global
-                indices = [index0, index]
-
-            elif name == 'y':
-                ptr = self.func.args[0]
-                indices = [index]
-
-            eptr = self.builder.gep(ptr, indices, name + "_" + v)
-            self.values[v] = eptr
-
-    def add_store(self, sequence, name):
-        for ix, v in enumerate(sequence):
-            index = ll.IntType(64)(ix)
-
-            if name == 'variables':
-                ptr = var_global
-                indices = [index0, index]
-
-            eptr = builder.gep(ptr, indices)
-            self.builder.store(self.builder.load(self.values[v]), eptr)
-
-    def add_call(self):
-        pass
 
     def add_mapping(self):
         pass
---
+++ tests/test_generate_llvm.py
@@ -0,0 +1,14 @@
+
+
+
+def test_llvm_1_mapping():
+   llvm_program =
+
+def test_llvm_1_function():
+    llvm_program =
+
+def test_llvm_1_function_and_mapping():
+    llvm_program =
+
+def test_llvm_1_loop_operation():
+    llvm_program =
