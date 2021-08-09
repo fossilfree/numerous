00ecbc98d86065a54e18da3b6dfe186863087a4f
artem <artem.chupryna@energymachines.com>
2021-08-09 14:41:08 +0200

    deleted unfinished test

--- tests/test_equation_statments/test_if_statement.py
+++ tests/test_equation_statments/test_if_statement.py
@@ -47,26 +47,6 @@ class IfTest2(EquationBase, Item):
 
 import numpy as np
 
-class IfTest4(EquationBase, Item):
-    def __init__(self, tag="tm"):
-        super(IfTest4, self).__init__(tag)
-        self.add_parameter('x_f_i', 5)
-        self.add_parameter('m_flow_in', 5)
-        self.add_parameter('T', 5)
-        self.add_parameter('h_f_i', 5)
-        self.add_parameter('h_we', 5)
-        self.add_parameter('c_pa', 5)
-        self.add_parameter('c_pw', 5)
-        self.add_parameter('p_tot', 5)
-        self.add_parameter('Rg', 5)
-        self.add_parameter('rh_cap', 5)
-        mechanics = self.create_namespace('test_nm')
-        mechanics.add_equations([self])
-
-    @Equation()
-    def eval(self, scope):
-
-
 
 class IfTest3(EquationBase, Item):
     def __init__(self, tag="tm"):
@@ -129,11 +109,4 @@ def test_external_if_statement_if_body_skipped(solver, use_llvm):
     assert s.model.historian_df['if_system2.tm1.test_nm.T_i4'][1] == expected_ti4
 
 
-@pytest.mark.parametrize("solver", solver_types)
-@pytest.mark.parametrize("use_llvm", [True, False])
-def test_external_if_statement_cycle_dependancy(solver, use_llvm):
-    model_ = model.Model(IfSystem('if_system3', IfTest4('tm1')), use_llvm=use_llvm)
-    s = simulation.Simulation(model_, solver_type=solver, t_start=0, t_stop=10.0, num=10, num_inner=10)
-    s.solve()
-    # expected_ti4 = 0
-    # assert s.model.historian_df['if_system2.tm1.test_nm.T_i4'][1] == expected_ti4
+
