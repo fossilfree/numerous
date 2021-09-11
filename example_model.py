ff407fda03f214685ffdd72c7105f014ebb5f3ce
amipy <46629183+amipy@users.noreply.github.com>
2021-09-11 18:40:15 +0200

    add scalable model

--- example_model.py
+++ example_model.py
@@ -1,24 +1,25 @@
-import pytest
-from numerous.engine.model.external_mappings import ExternalMappingElement
-
-from numerous.utils.data_loader import InMemoryDataLoader
-from pytest import approx
 from enum import Enum
 
-from numerous.engine.model.external_mappings.interpolation_type import InterpolationType
 from numerous.engine.model import Model
 from numerous.engine.simulation import Simulation
 
-from numerous.engine.system import Subsystem, ConnectorItem, Item, ConnectorTwoWay
-from numerous import EquationBase, Equation
-from numerous.engine.simulation.solvers.base_solver import solver_types
+from numerous.engine.system import Subsystem, Item
 from tests.test_equations import TestEq_ground, Test_Eq, TestEq_input
+from enum import Enum
+
+from numerous.engine.model import Model
+from numerous.engine.simulation import Simulation
+from numerous.engine.system import Subsystem, Item
+from tests.test_equations import TestEq_ground, Test_Eq, TestEq_input
+
 
 class SolverType(Enum):
     SOLVER_IVP = 0
     NUMEROUS = 1
 
+
 solver_types = [SolverType.NUMEROUS, SolverType.SOLVER_IVP]
+
 
 def ms2():
     class I(Item):
@@ -75,10 +76,78 @@ def ms2():
 
     return S2('S2')
 
-def test_chain_item_model(ms2, solver, use_llvm):
-    #print(type(ms2))
-    m1 = Model(system = ms2, use_llvm=use_llvm)
+
+def run_model(ms, solver, use_llvm):
+    # print(type(ms2))
+    m1 = Model(system=ms, use_llvm=use_llvm)
     s1 = Simulation(m1, t_start=0, t_stop=1000, num=10, solver_type=solver)
     s1.solve()
+    return s1
 
-test_chain_item_model(ms2(), solver= SolverType.NUMEROUS  , use_llvm=[True,False])
+
+def ms2N(n):
+    class I(Item):
+        def __init__(self, tag, P, T, R):
+            super().__init__(tag)
+
+            t1 = self.create_namespace('t1')
+            t1.add_equations([TestEq_input(P=P, T=T, R=R)])
+
+    class T(Item):
+        def __init__(self, tag, T, R):
+            super().__init__(tag)
+
+            t1 = self.create_namespace('t1')
+            t1.add_equations([Test_Eq(T=T, R=R)])
+
+    class G(Item):
+        def __init__(self, tag, TG, RG):
+            super().__init__(tag)
+            t1 = self.create_namespace('t1')
+            t1.add_equations([TestEq_ground(TG=TG, RG=RG)])
+
+    class S2N(Subsystem):
+        def __init__(self, tag):
+            super().__init__(tag)
+            items = []
+            input = I('1', P=100, T=0, R=10)
+            for i in range(n):
+                items.append(T(str(i+2), T=1, R=5))
+                #print(items)
+            #print(f'GNDID{n + 2}')
+            ground = G(str(n + 2), TG=10, RG=2)
+
+            input.t1.T_o.add_mapping(items[0].t1.T)
+
+            # item1.bind(input=input, output=item2)
+
+            for item in range(n):
+                if item == 0:
+                    items[item].t1.R_i.add_mapping(input.t1.R)
+                    #items[item].t1.T_i.add_mapping(input.t1.T)
+                    items[item].t1.T_o.add_mapping(items[item + 1].t1.T)
+                elif item == n-1:
+                    items[item].t1.R_i.add_mapping(items[item - 1].t1.R)
+                    items[item].t1.T_i.add_mapping(items[item - 1].t1.T)
+                    items[item].t1.T_o.add_mapping(ground.t1.T)
+                else:
+                    items[item].t1.R_i.add_mapping(items[item - 1].t1.R)
+                    items[item].t1.T_i.add_mapping(items[item - 1].t1.T)
+                    items[item].t1.T_o.add_mapping(items[item + 1].t1.T)
+
+            r_items = [input]
+            for i in items:
+                r_items.append(i)
+            r_items.append(ground)
+            print(r_items)
+            #print(type(items[0]))
+            self.register_items(r_items)
+
+    return S2N('S2')
+
+
+#s1=run_model(ms2(), solver=SolverType.NUMEROUS, use_llvm=[True, False])
+#s1_result=s1.model.historian_df
+
+s1n=run_model(ms2N(8), solver=SolverType.NUMEROUS, use_llvm=[True, False])
+s1n_result=s1n.model.historian_df
