449cd0e32b535613cdd3e7819811989dc378ea53
amipy <46629183+amipy@users.noreply.github.com>
2021-09-15 20:59:40 +0200

    Removed commented code and moved repeated classes

--- example_model.py
+++ example_model.py
@@ -1,10 +1,4 @@
-from enum import Enum
 
-from numerous.engine.model import Model
-from numerous.engine.simulation import Simulation
-
-from numerous.engine.system import Subsystem, Item
-from tests.test_equations import TestEq_ground, Test_Eq, TestEq_input
 from enum import Enum
 
 from numerous.engine.model import Model
@@ -21,28 +15,30 @@ class SolverType(Enum):
 solver_types = [SolverType.NUMEROUS, SolverType.SOLVER_IVP]
 
 
+class I(Item):
+    def __init__(self, tag, P, T, R):
+        super().__init__(tag)
+
+        t1 = self.create_namespace('t1')
+        t1.add_equations([TestEq_input(P=P, T=T, R=R)])
+
+
+class T(Item):
+    def __init__(self, tag, T, R):
+        super().__init__(tag)
+
+        t1 = self.create_namespace('t1')
+        t1.add_equations([Test_Eq(T=T, R=R)])
+
+
+class G(Item):
+    def __init__(self, tag, TG, RG):
+        super().__init__(tag)
+
+        t1 = self.create_namespace('t1')
+        t1.add_equations([TestEq_ground(TG=TG, RG=RG)])
+
 def ms2():
-    class I(Item):
-        def __init__(self, tag, P, T, R):
-            super().__init__(tag)
-
-            t1 = self.create_namespace('t1')
-            t1.add_equations([TestEq_input(P=P, T=T, R=R)])
-
-    class T(Item):
-        def __init__(self, tag, T, R):
-            super().__init__(tag)
-
-            t1 = self.create_namespace('t1')
-            t1.add_equations([Test_Eq(T=T, R=R)])
-
-    class G(Item):
-        def __init__(self, tag, TG, RG):
-            super().__init__(tag)
-
-            t1 = self.create_namespace('t1')
-            t1.add_equations([TestEq_ground(TG=TG, RG=RG)])
-
     class S2(Subsystem):
         def __init__(self, tag):
             super().__init__(tag)
@@ -56,13 +52,10 @@ def ms2():
 
             input.t1.T_o.add_mapping(item1.t1.T)
 
-            # item1.bind(input=input, output=item2)
 
             item1.t1.R_i.add_mapping(input.t1.R)
             item1.t1.T_i.add_mapping(input.t1.T)
             item1.t1.T_o.add_mapping(item2.t1.T)
-            #  t_0 = item1.t1.T_o
-            # item1.t1.T_o = item2.t1.T
 
             item2.t1.R_i.add_mapping(item1.t1.R)
             item2.t1.T_i.add_mapping(item1.t1.T)
@@ -86,26 +79,6 @@ def run_model(ms, solver, use_llvm):
 
 
 def ms2N(n):
-    class I(Item):
-        def __init__(self, tag, P, T, R):
-            super().__init__(tag)
-
-            t1 = self.create_namespace('t1')
-            t1.add_equations([TestEq_input(P=P, T=T, R=R)])
-
-    class T(Item):
-        def __init__(self, tag, T, R):
-            super().__init__(tag)
-
-            t1 = self.create_namespace('t1')
-            t1.add_equations([Test_Eq(T=T, R=R)])
-
-    class G(Item):
-        def __init__(self, tag, TG, RG):
-            super().__init__(tag)
-            t1 = self.create_namespace('t1')
-            t1.add_equations([TestEq_ground(TG=TG, RG=RG)])
-
     class S2N(Subsystem):
         def __init__(self, tag):
             super().__init__(tag)
@@ -113,18 +86,14 @@ def ms2N(n):
             input = I('1', P=100, T=0, R=10)
             for i in range(n):
                 items.append(T(str(i+2), T=1, R=5))
-                #print(items)
-            #print(f'GNDID{n + 2}')
             ground = G(str(n + 2), TG=10, RG=2)
 
             input.t1.T_o.add_mapping(items[0].t1.T)
 
-            # item1.bind(input=input, output=item2)
 
             for item in range(n):
                 if item == 0:
                     items[item].t1.R_i.add_mapping(input.t1.R)
-                    #items[item].t1.T_i.add_mapping(input.t1.T)
                     items[item].t1.T_o.add_mapping(items[item + 1].t1.T)
                 elif item == n-1:
                     items[item].t1.R_i.add_mapping(items[item - 1].t1.R)
@@ -139,15 +108,10 @@ def ms2N(n):
             for i in items:
                 r_items.append(i)
             r_items.append(ground)
-            print(r_items)
-            #print(type(items[0]))
             self.register_items(r_items)
 
     return S2N('S2')
 
 
-#s1=run_model(ms2(), solver=SolverType.NUMEROUS, use_llvm=[True, False])
-#s1_result=s1.model.historian_df
-
-s1n=run_model(ms2N(8), solver=SolverType.NUMEROUS, use_llvm=[True, False])
+s1n=run_model(ms2N(5), solver=SolverType.NUMEROUS, use_llvm=[True, False])
 s1n_result=s1n.model.historian_df
