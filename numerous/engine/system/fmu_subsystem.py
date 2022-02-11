7419a9d3fefdcf95e35f55d6621491920532e569
artem <artem.chupryna@energymachines.com>
2022-02-11 17:33:13 +0100

    work in progress

--- numerous/engine/system/fmu_subsystem.py
+++ numerous/engine/system/fmu_subsystem.py
@@ -304,14 +304,20 @@ class FMU_Subsystem(Subsystem, EquationBase):
         event_action_2 = namespace["event_action_2"]
         event_action_2.lines = ast.unparse(ast.Module(body=[b1], type_ignores=[]))
         gec = generate_eq_call(deriv_names_ordered, var_names_ordered)
+        if debug_output:
+            print(ast.unparse(gec))
+        code = compile(ast.parse(ast.unparse(gec)), filename='fmu_eval', mode='exec')
+        namespace = {"Equation": Equation}
+        exec(code, namespace)
+        result = namespace["eval"]
+        result.lines = ast.unparse(gec)
+        result.lineno = ast.parse(ast.unparse(gec)).body[0].lineno
+        self.eval = ptypes.MethodType(result, self)
+        self.equations.append(self.eval)
         self.t1 = self.create_namespace(namespace_)
         self.t1.add_equations([self])
         self.add_event("event", event_cond_2, event_action_2, compiled_functions={"event_cond": event_cond,
                                                                                   "event_action": event_action})
-
-    @Equation()
-    def eval(self, scope):
-            (scope.x0_dot, scope.x1_dot) = self.fmu_eval(scope.x0, scope.x1, scope.mu)
 
     def set_variables(self, model_description):
         for variable in model_description.modelVariables:
@@ -359,7 +365,7 @@ class S3(Subsystem):
     def __init__(self, tag):
         super().__init__(tag)
 
-        fmu_filename = 'VanDerPol.fmu'
+        fmu_filename = 'bouncingBall.fmu'
         fmu_subsystem = FMU_Subsystem(fmu_filename, "VanDerPol", debug_output=True)
         # fmu_filename = 'bouncingBall.fmu'
         # fmu_subsystem2 = FMU_Subsystem(fmu_filename, "BouncingBall2", debug_output=True)
@@ -373,14 +379,14 @@ class S3(Subsystem):
 subsystem1 = S3('q1')
 m1 = Model(subsystem1, use_llvm=False)
 s = Simulation(
-    m1, t_start=0, t_stop=10, num=100, num_inner=100, max_step=.1, solver_type=SolverType.SOLVER_IVP)
+    m1, t_start=0, t_stop=1, num=100, num_inner=100, max_step=.1, solver_type=SolverType.SOLVER_IVP)
 # sub_S = m1.system.get_item(ItemPath("q1.BouncingBall"))
 s.solve()
 # sub_S.fmu.terminate()
 
 fig, ax = plt.subplots()
 # t = np.linspace(0, 1.0, 100 + 1)
-y = np.array(m1.historian_df["q1.VanDerPol.t1.x1"])
+y = np.array(m1.historian_df["q1.VanDerPol.t1.h"])
 # y2 = np.array(m1.historian_df["q1.VanDerPol.t1.x1"])
 # y2 = np.array(m1.historian_df["q1.BouncingBall2.t1.h"])
 # y3 = np.array(m1.historian_df["q1.BouncingBall3.t1.h"])
