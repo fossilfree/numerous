076d7cd35d9ecf1935a35d22e3417f72aca2143c
miamirov <mikkiamirov17@gmail.com>
2022-03-14 22:31:35 +0300

    linters

--- linters/get_base_branch.py
+++ linters/get_base_branch.py
@@ -1,11 +1,14 @@
 import os
 import requests
 
-url = os.getenv('CIRCLE_PULL_REQUEST', 'https://github.com/fossilfree/numerous/pull/109')
+url = os.getenv('CIRCLE_PULL_REQUEST')
+if url is None:
+    print('develop')
+else:
+    _, _, _, user, repo, _, num = url.split('/')
 
-_, _, _, user, repo, _, num = url.split('/')
+    new_url = f'https://api.github.com/repos/{user}/{repo}/pulls/{num}'
+    base_branch = requests.get(new_url).json().get('base', {}).get('ref', 'master')
 
-new_url = f'https://api.github.com/repos/{user}/{repo}/pulls/{num}'
-base_branch = requests.get(new_url).json().get('base', {}).get('ref', 'master')
+    print(base_branch)
 
-print(base_branch)
