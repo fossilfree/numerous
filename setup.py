e8b3bb7dfca3ccaaec30db636c8425c98f6d878c
miamirov <mikkiamirov17@gmail.com>
2022-03-14 03:36:15 +0300

    pypi automatic

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="numerous-engine_test",
+    name="numerous-engine-test",
     version="0.2",
     author='Artem Chupryna, EnergyMachines ApS',
     author_email='artem.chupryna@energymachines.com',
