e1216531edb353b894f935130bdfa8e09108f2b5
artem <artem.chupryna@energymachines.com>
2022-04-06 13:28:32 +0200

    refactoring #1

--- examples/FMU_usage/VanDerPol_test_FMU.py
+++ examples/FMU_usage/VanDerPol_test_FMU.py
@@ -31,32 +31,26 @@ class G(Item):
 class S3(Subsystem):
     def __init__(self, tag):
         super().__init__(tag)
-        fmu_filename = '/home/artem/Source/FMPy/Rectifier.fmu'
-        # fmu_filename = 'VanDerPol.fmu'
-        fmu_subsystem = FMU_Subsystem(fmu_filename, "Rectifier", debug_output=True)
+        fmu_filename = 'VanDerPol.fmu'
+        fmu_subsystem = FMU_Subsystem(fmu_filename, "VanDerPol", debug_output=True)
         self.register_items([fmu_subsystem])
 
 
 subsystem1 = S3('q1')
-m1 = Model(subsystem1, use_llvm=False)
+m1 = Model(subsystem1, use_llvm=True)
 s = Simulation(
-    m1, t_start=0, t_stop=0.1, num=1000, num_inner=1, max_step=.1, solver_type=SolverType.SOLVER_IVP)
-# sub_S = m1.system.get_item(ItemPath("q1.BouncingBall"))
+    m1, t_start=0, t_stop=10, num=1000, num_inner=1, max_step=.1, solver_type=SolverType.NUMEROUS)
+
 s.solve(run_fmu_event_action=True)
-# sub_S.fmu.terminate()
+
 
 fig, ax = plt.subplots()
-# t = np.linspace(0, 1.0, 100 + 1)
-y = np.array(m1.historian_df["q1.Rectifier.t1.Rectifier1_Capacitor2_v"])
-# y = np.array(m1.historian_df["q1.Rectifier.t1.x1"])
-# y2 = np.array(m1.historian_df["q1.BouncingBall2.t1.h"])
-# y3 = np.array(m1.historian_df["q1.BouncingBall3.t1.h"])
+y = np.array(m1.historian_df["q1.VanDerPol.t1.x1"])
 t = np.array(m1.historian_df["time"])
 ax.plot(t, y)
-# ax.plot(t, y2)
-# ax.plot(t, y3)
 
-ax.set(xlabel='time (s)', ylabel='outputs', title='BB')
+
+ax.set(xlabel='time (s)', ylabel='outputs', title='VanDerPol')
 ax.grid()
 
 plt.show()
