66ebd1d578258052ed3ea8de27dd21bed5c18b5c
artem <artem.chupryna@energymachines.com>
2022-05-09 14:19:33 +0200

    work in progress

--- examples/FMU_usage/example_FMU.py
+++ examples/FMU_usage/example_FMU.py
@@ -39,7 +39,7 @@ class S3(Subsystem):
 subsystem1 = S3('q1')
 m1 = Model(subsystem1, use_llvm=True)
 s = Simulation(
-    m1, t_start=0, t_stop=5, num=10, num_inner=1, max_step=.1)
+    m1, t_start=0, t_stop=5, num=1000, num_inner=1, max_step=.1)
 
 s.solve()
 
