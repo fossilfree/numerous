c6c9d6a714d262eb93f7b84d88fc3dcb621259e6
artem <artem.chupryna@energymachines.com>
2022-05-30 17:50:40 +0200

    update

--- numerous/engine/simulation/solvers/numerous_solver/numerous_solver.py
+++ numerous/engine/simulation/solvers/numerous_solver/numerous_solver.py
@@ -264,14 +264,14 @@ class Numerous_solver(BaseSolver):
                             t_r = t_m
                         elif np.sign(e_r) != np.sign(e_m):
                             t_l = t_m
-                        if abs(e_m) < 1e-6:
+                        if abs(e_m) < 1e-6 or abs(t_l - t_r) < 1e-6:
                             status = 1
                         if i > imax:
                             status = -1
                         t_m = (t_l + t_r) / 2
                         y_m = sol(t_m, t, y)
 
-                    return status, t_m, y_m
+                    return status, t_r, sol(t_r, t, y)
 
                 if step_converged:
                     g_new = events(t, get_variables_modified(y))
@@ -281,7 +281,7 @@ class Numerous_solver(BaseSolver):
                     g = g_new
 
                     for ix in np.concatenate((np.argwhere(up), np.argwhere(down))):
-                        eps = 0.0001  # for case to t_event = t
+                        eps = 1.e-6 # for case to t_event = t
                         status, t_event, y_event = check_event(events, ix[0],
                                                                t_previous, y_previous, t, y)
                         t_events[ix[0]] = t_event - eps
