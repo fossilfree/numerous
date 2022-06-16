0fffbbee695f5c8bd498b80f3de427b92bebefaf
artem <artem.chupryna@energymachines.com>
2022-06-16 17:09:13 +0200

    work in progress

--- examples/FMU_usage/example_FMU.py
+++ examples/FMU_usage/example_FMU.py
@@ -8,12 +8,11 @@ from numerous.engine.system import Item, Subsystem
 from numerous.engine.system.fmu_subsystem import FMU_Subsystem
 
 
-
 class S3(Subsystem):
     def __init__(self, tag):
         super().__init__(tag)
 
-        fmu_filename = '/home/artem/fmus/PCU_RHU_EnergyMachines_01_modex_Linux.fmu'
+        fmu_filename = '_01_modex_Linux.fmu'
         fmu_subsystem = FMU_Subsystem(fmu_filename, "PCU_RHU", debug_output=True)
         # fmu_subsystem.t1.variables["h"].value = 19
         self.register_items([fmu_subsystem])
@@ -32,7 +31,6 @@ fig, ax = plt.subplots()
 y1 = np.array(m1.historian_df['q1.PCU_RHU.t1.P_out'])
 t = np.array(m1.historian_df["time"])
 ax.plot(t, y1)
-
 
 ax.set(xlabel='time (s)', ylabel='outputs', title='Something')
 ax.grid()
