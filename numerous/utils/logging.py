b471229ba598edcf74ee11f47af2546b72ab963b
miamirov-teracloud <105202371+miamirov-teracloud@users.noreply.github.com>
2022-08-11 16:30:49 +0200

    custom logger (#177)
    * custom logger
    
    * Update logging.py
    
    * Update __init__.py
    
    Co-authored-by: miamirov <mikkamirov17@gmail.com>

---
+++ numerous/utils/logging.py
@@ -0,0 +1,11 @@
+import logging
+
+logger = logging.getLogger('numerous_logger')
+
+formatter = logging.Formatter(fmt='%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+handler = logging.StreamHandler()
+handler.setFormatter(formatter)
+
+logger.addHandler(handler)
+logger.setLevel(logging.INFO)
