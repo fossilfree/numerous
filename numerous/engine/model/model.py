d91a862b53d4c426ef9bd0393ccbf5352f146bc4
artem-chupryna <artem.chupryna@energymachines.com>
2022-10-17 15:27:59 +0200

    removed duplicate top_item assign (#190)
    * removed duplicate top_item assign
    
    * removed duplicate top_item assign

--- numerous/engine/model/model.py
+++ numerous/engine/model/model.py
@@ -111,7 +111,6 @@ class ModelAssembler:
         return variables_, equation_dict
 
 
-
 class Model:
     """
      The model object traverses the system to collect all information needed to pass to the solver
@@ -257,8 +256,6 @@ class Model:
             self.variables.update(variables)
 
         mappings = []
-        for variable in self.variables.values():
-            variable.top_item = self.system.id
 
         for scope_var_idx, var in enumerate(self.variables.values()):
             if var.mapping:
