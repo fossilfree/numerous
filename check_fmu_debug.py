87eff2318d461b19df451dace381abf85e78c6c3
miamirov <mikkamirov17@gmail.com>
2022-11-14 16:55:15 +0100

    check fmu

--- check_fmu_debug.py
+++ check_fmu_debug.py
@@ -7,7 +7,8 @@ from numerous.engine.system import Subsystem
 from numerous.engine.system.fmu_subsystem import FMU_Subsystem
 from fmpy import read_model_description
 
-model_names = ['BouncingBall', 'Dahlquist', 'Feedthrough', 'Stair', 'VanDerPol']
+
+model_names = ['VanDerPol']
 
 
 class S3(Subsystem):
@@ -20,22 +21,24 @@ class S3(Subsystem):
 
 
 for model_name in model_names:
-    fmu_path = f'build_tmp/dist/{model_name}.fmu'
-    description = read_model_description(fmu_path)
+    description = read_model_description(f'{model_name}.fmu')
     step_size = description.defaultExperiment.stepSize
     start_time = description.defaultExperiment.startTime
     end_time = description.defaultExperiment.stopTime
     num = int((end_time - start_time) / step_size)
-    for use_llvm in [True, False]:
-        ref = pandas.read_csv(f'build_tmp/temp/{model_name}/documentation/{model_name}_ref.csv')
-        system = S3('q1', fmu_path)
+    system = S3('q1', f'{model_name}.fmu')
+    for use_llvm in [False]:
         m1 = Model(system, use_llvm=use_llvm)
         s = Simulation(
             m1, t_start=start_time, t_stop=end_time, num=num)
         s.solve()
-
+        ref = pandas.read_csv(f'{model_name}_ref.csv')
+        print(m1.historian_df)
+        print(m1.historian_df.aliases)
         for col in ref.columns:
             if col == 'time':
                 continue
             else:
-                assert np.allclose(ref[col], np.array(m1.historian_df[f'q1.q1.t1.{col}']), rtol=1e-3)
+                print(ref[col])
+                print(np.array(m1.historian_df[f'q1.q1.t1.{col}']))
+                assert np.allclose(ref[col], np.array(m1.historian_df[f'q1.q1.t1.{col}']), rtol=0.2)
