5f39475881af93bf4f19c239a45532227f4bd584
Tobias Dokkedal Elmøe <57899579+mrdobalina2k@users.noreply.github.com>
2022-11-17 13:54:16 +0100

    Numerous solver split (#192)
    * about to completely split numerous solver out of numerous engine.
    
    * feat: split out numerous solver and install via requirements. Update tests accordingly. Add documentation in docstrings.
    
    * fix: add missing method to solver_interface. Reintroduce "reset" method on simulation class.
    chore(simulation): remove unnecessary numba_model/compiled model from Simulation class.
    tests(test_external_data, test_bouncing_ball_event_item_level): add back uncommented tests, and add higher tolerance as default tolerance is not enough for solver.
    
    * fix(test_numerous_engine_solver_interface): add t_start to sim.reset()

--- tests/test_fmu/test_bouncing_ball/test_bouncing_ball.py
+++ tests/test_fmu/test_bouncing_ball/test_bouncing_ball.py
@@ -4,7 +4,6 @@ import pytest
 
 from numerous.engine.model import Model
 from numerous.engine.simulation import Simulation
-from numerous.engine.simulation.solvers import solver_types
 from numerous.engine.system import Subsystem
 from numerous.engine.system.fmu_subsystem import FMU_Subsystem
 
@@ -19,13 +18,12 @@ class S3(Subsystem):
 
 
 @pytest.mark.xfail
-@pytest.mark.parametrize("solver", solver_types)
 @pytest.mark.parametrize("use_llvm", [True, False])
 def test_bouncing_ball(solver, use_llvm):
     subsystem1 = S3('q1')
     m1 = Model(subsystem1, use_llvm=use_llvm)
     s = Simulation(
-        m1, t_start=0, t_stop=20, num=200, num_inner=1000, max_step=.1, solver_type=solver)
+        m1, t_start=0, t_stop=20, num=200, num_inner=1000, max_step=.1)
     s.solve()
     h = np.array(m1.historian_df["q1.BouncingBall.t1.h"])
     v = np.array(m1.historian_df["q1.BouncingBall.t1.v"])
