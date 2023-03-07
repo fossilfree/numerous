d22d72e739c9d97dfa13f09b660587d4d14cd490
artem-chupryna <artem.chupryna@energymachines.com>
2023-03-07 13:10:21 +0100

    version 0.4.1

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open('requirements.txt', "r") as requirements:
 
 setuptools.setup(
     name="numerous-engine",
-    version="0.4.0",
+    version="0.4.1",
 
     author='Artem Chupryna, EnergyMachines ApS',
     author_email='artem.chupryna@energymachines.com',
